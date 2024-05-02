e05e29276cc6808a650180c8d95a2f9cba41d1f1
Khizrabatool <s.khiza.rizvi@gmail.com>
2024-05-02 08:10:28 +0500

    initial commit

---
+++ assign03/nameCases.js
@@ -0,0 +1,20 @@
+var nam = 'syed ZEEshan haider';
+// // //Lower case
+console.log(nam.toLowerCase());
+// // //Upper case
+console.log(nam.toUpperCase());
+// //Title Case Method 1
+var newName = nam.split(" ").map(function (l) { return l[0].toUpperCase() +
+    l.substr(1); }).join(" ");
+console.log(newName);
+// //Title Case Method 2
+function TitleCase(str) {
+    str = str.toLowerCase();
+    str = str.split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+}
+console.log(TitleCase(nam));
+//console.log(TitleCase)
