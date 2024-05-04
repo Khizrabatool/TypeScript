4b89a9fa4aa52c0788bc8d5c5e57367cb9f200e2
Khizrabatool <s.khiza.rizvi@gmail.com>
2024-05-04 15:23:18 +0500

    initial commit

---
+++ assign07/number8.ts
@@ -0,0 +1,11 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+//Be sure to enclose your operations in print statements to see the results. 
+
+
+ let a =8
+ 
+ console.log('Add a + a = ', a+a)
+ console.log('multiply a * a = ', a*a)
+ console.log('Subract a - a = ', a-a)
+ console.log('Division a / a = ', a/a)
+ console.log('Above operation consolidated result = ', a+a-a*a/a)
