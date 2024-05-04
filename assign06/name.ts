693fd62f20a0e04624edbf85aee4dcd8720ee624
Khizrabatool <s.khiza.rizvi@gmail.com>
2024-05-04 15:21:31 +0500

    initial commit

---
+++ assign06/name.ts
@@ -0,0 +1,10 @@
+let user_name ='  Zeeshan Haider   ';
+console.log('String')
+console.log(user_name, user_name.length)
+
+let result1=user_name.trim()
+
+console.log('\nTrimed String\n______________')
+console.log(result1, result1.length)
+
+
