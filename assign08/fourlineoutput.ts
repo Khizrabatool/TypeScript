b80af4f6b7b855b3c3a9da1119f73a400e7102e5
Khizrabatool <s.khiza.rizvi@gmail.com>
2024-05-04 15:24:24 +0500

    initial commit

---
+++ assign08/fourlineoutput.ts
@@ -0,0 +1,21 @@
+/* 
+You should create four lines that look like this:
+
+_____________________________________________
+
+console.log(5 + 3)
+
+_____________________________________________
+
+Your output should simply be four lines with the number 8 appearing once on each line.
+*/
+console.log('-'.repeat(40))
+console.log(5+3)
+console.log('+'.repeat(40))
+console.log(4*2)
+console.log('='.repeat(40))
+console.log(16/2)
+console.log('.'.repeat(40))
+console.log(4*2)
+
+
