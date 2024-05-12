fe9a1d0abe6c473cf1ff9cad6d3985ac334f0799
Khizrabatool <s.khizra.rizvi@gmail.com>
2024-05-12 10:45:46 +0500

    initial commit

---
+++ Function/main.ts
@@ -0,0 +1,95 @@
+// function greet() {
+//   return "hello world";
+// }
+
+// let message = greet();
+// console.log(message);
+
+// function sum() {
+//   return 2 + 2;
+// }
+
+// let result = sum();
+// console.log(result);
+
+// function  sum(num1: number, num2: number) {
+//     return num1 + num2
+// }
+// let result =sum(4,4);
+// let result2 =sum(8,8);
+// let result3 =sum(5,5);
+
+
+// console.log(result)
+// console.log(result2)
+// console.log(result3)
+
+//Arrow function
+// let greet=(name: string) => `Hello ${name}`
+// console.log(greet('Okasha'))
+
+
+// Zeeshan Haider <hyder.zeeshan110@gmail.com>
+// Thu, May 9, 7:33 PM (2 days ago)
+// to Syed.zeeshanhaider, me
+
+
+
+// // multiplication function
+// function multiply(num1: number, num2: number) {
+//   return num1 * num2;
+// }
+
+// let multiResult = multiply(2, 2);
+
+// console.log(multiResult);
+
+// // Division Function
+// function divide(num1: number, num2: number) {
+//   return num1 / num2;
+// }
+// let divideResult = divide(4, 2);
+// console.log(divideResult);
+
+// // subtraction function
+// function subtraction(num1: number, num2: number) {
+//   return num1 - num2;
+// }
+// let subtractionResult = divide(4, 2);
+// console.log(subtractionResult);
+
+// // default parameters function
+
+// function userGreet(name: string = "user") {
+//   console.log(`Hello ${name}`);
+// }
+
+// userGreet(`Okasha`);
+// userGreet();
+
+// function usrAge(age: number, name: string = "User") {
+//   console.log(`Hello ${name} your age is ${age}`);
+// }
+
+// usrAge (20)
+
+// arrow function
+
+// let greet = (name: string) => {
+//     console.log(`hello ${name}`);
+// }
+
+// greet(`Okasha`);
+
+// let greet = (name: string) => `hello ${name}`;
+// let result = greet("Okasha")
+// console.log(result);
+
+let num = 10
+let evenNum = (num: number) =>  num % 2 === 0 ? "Even":"Odd"
+
+console.log(evenNum(12));
+
+// Homework
+
+// functions / arrow functions / global variable / local variable / hoisting
