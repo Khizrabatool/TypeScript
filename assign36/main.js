5fabfe8469dc5e986682542b9ea615619d3d938a
Khizrabatool <s.khizra.rizvi@gmail.com>
2024-05-19 01:06:44 +0500

    initial commit

---
+++ assign36/main.js
@@ -0,0 +1,10 @@
+/*
+36. T-Shirt: Write a function called make_shirt() that accepts a size and the
+text of a message that should be printed on the shirt. The function should print
+a sentence summarizing the size of the shirt and the message printed on it.
+Call the function.
+*/
+function make_shrit(size, text) {
+    console.log("Size of this shirt is ".concat(size, " and text printed on it is ").concat(text));
+}
+make_shrit("mediumn", "Let's do it");
---
+++ assign36/main.ts
@@ -0,0 +1,14 @@
+/*
+36. T-Shirt: Write a function called make_shirt() that accepts a size and the
+text of a message that should be printed on the shirt. The function should print
+a sentence summarizing the size of the shirt and the message printed on it.
+Call the function.
+*/
+
+function make_shrit(size: String, text: string) {
+ console.log(`Size of this shirt is ${size} and text printed on it is ${text}`);
+    
+
+}
+
+make_shrit("mediumn","Let's do it")
